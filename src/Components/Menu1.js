ec6dc1cce0c87436f679cff21725a44ae451969a
Muzided <muzammillone07@gmail.com>
2024-01-30 06:25:15 +0500

    1/30/24

--- src/Components/Menu1.js
+++ src/Components/Menu1.js
@@ -5,7 +5,7 @@ const Menu1 = () => {
   return (
     <div className='flex flex-col md:top-[50px] md:bg-white px-4 md:p-4  gap-8 md:absolute md:shadow-lg md:rounded-sm '>
   <h1 className='text-cGreen cursor-pointer hover:text-[#21C063] font-bold text-sm'> Doctor </h1>
-  <h1 className='text-cGreen cursor-pointer hover:text-[#21C063] font-bold text-sm'> Single Profile </h1>
+  <h1 className=' text-cGreen cursor-pointer hover:text-[#21C063] font-bold text-sm'> Single Profile </h1>
     </div>
   )
 }
